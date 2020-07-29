309ab8b3758562607ec70036278d3c77b58f6fae
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2020-07-29 18:31:56 -0300

    estrutura base criada

---
+++ jogo.js
@@ -0,0 +1,7 @@
+console.log('[Daniel Oliveira] FlappyBird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
